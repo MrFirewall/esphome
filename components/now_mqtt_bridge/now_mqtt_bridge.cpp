1de7a4798c092ef9a58214738c9d12d0b40ceb7a
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2024-05-25 16:51:23 +0200

    Update now_mqtt_bridge.cpp

--- components/now_mqtt_bridge/now_mqtt_bridge.cpp
+++ components/now_mqtt_bridge/now_mqtt_bridge.cpp
@@ -49,7 +49,7 @@ namespace esphome
 
             // check for binary_sensor or sensor
             message_type = tokens[2];
-            if
+            if (message_type.compare("binary_sensor") != 0)
             {
                 if (strlen(tokens[3]) != 0)
                 {
