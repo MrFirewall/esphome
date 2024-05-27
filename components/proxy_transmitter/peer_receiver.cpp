ae4d2f2f129920e512731f99eeba8fb84219963a
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2024-05-27 08:54:21 +0200

    Update peer_receiver.cpp

--- components/proxy_transmitter/peer_receiver.cpp
+++ components/proxy_transmitter/peer_receiver.cpp
@@ -198,7 +198,7 @@ namespace esphome
 				counter = counter++;
 				ESP_LOGD(TAG->get_tag(), "");
 			        ESP_LOGD(TAG->get_tag(), "----------------------");
-			        printf(counter);
+			        int printf(counter);
 			        ESP_LOGD(TAG->get_tag(), "----------------------");
 			}else {
 				go_to_sleep();
